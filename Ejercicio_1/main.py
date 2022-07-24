22c201284ab458c3f4353a97e54492ce1569191c
Leonardo E. Esteves Martins <69331303+Leoeze83@users.noreply.github.com>
2022-07-23 22:30:20 -0300

    corrigiendo errores

--- Ejercicio_1/main.py
+++ Ejercicio_1/main.py
@@ -2,6 +2,16 @@
 Consigna:
  Escribe un programa que pregunte al usuario su edad y muestre por pantalla si es mayor de edad o no.
 """
+# Solicito al usuario su edad
+# Valido si su type es str , vuelvo a solicitar el dato en type numerico
 
-
-
+print("Ingrese su edad y presione la tecla Enter")
+edad = input()
+if isinstance(edad, str):
+    print("La edad debe ser ingresada en números , por favor vuelva a introducirlo ")
+    edad = input()
+elif isinstance(edad, int):
+    if edad >= 21:
+        print("Usted tiene ", edad, " años. Es mayor de edad")
+    else:
+        print("Su edad es: ", edad, " años, usted no es mayor de edad")
